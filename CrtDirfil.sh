e3b133f7b01bfab16308f21b048a9adc4a01843c
r12w4n <48220263+r12w4n@users.noreply.github.com>
2019-03-21 16:50:04 +0530

    Create Directory and move to specific location
    Create directory write content on that copy to suitable location in your home

--- CrtDirfil.sh
+++ CrtDirfil.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
+#Create directory write content on that copy to suitable location in your home
+
+echo "Enter name for directory "
+read dname
+mkdir $dname
+cd $dname
+echo "enter a name for creating file "
+read file
+echo "Enter its extension "
+read ext
+touch $file.$ext
+echo "type anything to file "
+read content
+echo $content > $file.$ext
+cd ..
+echo "Where to save type path "
+read path
+mv $dname $path
+echo "Done!! Directory and File Creation and Moved to specific location"                                                                          
+
+
+
 
 # CreateDirectory > WriteContentsOnThat > CopyToSuitableLocationInHOme
 
-mkdir TheemDoc
-cd TheemDoc
-touch 1.txt 2.txt 3.txt
-cd ..
+# mkdir TheemDoc
+# cd TheemDoc
+# touch 1.txt 2.txt 3.txt
+# cd ..
 
 # $dest= "/home"
-cp -r TheemDoc /home #copying files to destination directory
-rm -rf TheemDoc
+# cp -r TheemDoc /home #copying files to destination directory
+# rm -rf TheemDoc
 
