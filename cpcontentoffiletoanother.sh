bb11f7203209c2eb8d4f2b5c5e0fd744bea2f6c9
r12w4n <48220263+r12w4n@users.noreply.github.com>
2019-03-21 16:09:35 +0530

    copy content of one file to another

--- cpcontentoffiletoanother.sh
+++ cpcontentoffiletoanother.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
+#copy content of one file to another
 
-#Copy Contents of one file to another
+echo "Enter a File Name to copy (source)"
+read f1
+echo "Enter a file name (destination)"
+read f2
 
-SRCFILE=$1
-DSTFILE=$2
+if [ -f $f1 ]
+then
+        cp $f1 $f2
+else
+        echo "File Does not Exist"
+fi
+                        
 
-touch $2
 
-cat $SRCFILE > $2
+
+#Another way to do it
+#SRCFILE=$1
+#DSTFILE=$2
+#touch $2
+#cat $SRCFILE > $2
