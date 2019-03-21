39788e5bd29f3d597a1bac66e21884405be1bb4f
r12w4n <48220263+r12w4n@users.noreply.github.com>
2019-03-21 23:14:28 +0530

    Use Pipeline and command substitution
    Use Pipeline and command substitution to set the length of a line in file to a variable

---
+++ pipleline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Enter a file name"
+read file
+if [ -f $file ] 
+then
+	echo "File found with a name $file"
+	echo "File content as below"
+while read line;
+do echo -n "$line" | wc -c; done < $file
+count=$(wc -l < $file)
+echo "Total Line in File: $count"
+else
+	echo "File not found check other file using running script"
+fi
+
